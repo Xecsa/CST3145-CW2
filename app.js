a3c999f099b4d4e7669da2545bdbd4777deb3fc9
Saba Shaikh <xecsaba@gmail.com>
2024-02-07 21:59:42 +0400

    Search implemented

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ app.use((req, res, next) => {
     next();
 });
 
+// Static file middleware for lesson images
+app.use((express.static('Public')));
+
 // Logger
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`);
@@ -23,7 +26,7 @@ app.use((req, res, next) => {
 });
 
 // Static file middleware for lesson images
-app.use((express.static('images')));
+// app.use(('/images',express.static('Public/images')));
 
 // Replace the placeholder URL with your actual MongoDB Atlas connection string
 const mongoURI = 'mongodb+srv://saba14:saba14@cluster0.dobilew.mongodb.net/lessonsHub?retryWrites=true&w=majority';
@@ -41,6 +44,15 @@ client.connect()
         const ordersCollection = db.collection('orders');
 
         // Define routes
+
+        app.get('/', (req,res,next) => {
+            res.send('Select a collection, e.g, /collection/messages')
+        })
+
+        app.param('', (req,res,next,collectionName) =>{
+            req.collection = db.collection(collectionName)
+            return next()
+        })
 
         // Route to get all lessons
         app.get('/lessons', async (req, res) => {
@@ -64,116 +76,75 @@ client.connect()
             }
         });
 
-        // Route to add a new lesson
-        app.post('/lessons', async (req, res) => {
-            try {
-                const { subject, price, location, spaces, image } = req.body;
-                const result = await lessonsCollection.insertOne({ subject, price, location, spaces, image });
-                res.json({ message: 'Lesson added', lessonId: result.insertedId });
-            } catch (error) {
-                console.error(error);
-                res.status(500).json({ error: 'Internal Server Error' });
-            }
-        });
 
         // Route to add a new order
         app.post('/orders', async (req, res) => {
             try {
-                const { name, phone, lessons } = req.body;
-                const result = await ordersCollection.insertOne({ name, phone, lessons });
+                // const { name, phone, lessons } = req.body;
+                // const result = await ordersCollection.insertOne({ name, phone, lessons });
+                const result = await ordersCollection.insertOne(req.body);
                 res.json({ message: 'Order added', orderId: result.insertedId });
+
             } catch (error) {
                 console.error(error);
                 res.status(500).json({ error: 'Internal Server Error' });
             }
         });
 
-        // Route to update available lesson space after an order
-        // app.put('/lessons/:id', (req, res, next) => {
-            // req.collection.update(
-                // { _id: new ObjectID(req.params.id) },
-                // {$set: req.body},
-                // { safe: true, multi: false },
-                // (e, result) => {
-                    // if (e) return next(e);
-                    // res.send((result.result.n === 1) ? { msg: 'success' } : { msg: 'error' })
-                // }
-            // );
-        // });
 
         app.put ('/lessons/:id', (req, res, next) => {
             req.collection = db.collection("lessons")
             req.collection.update(
                 {_id: new ObjectID(req.params.id)},
-                {$set: req.body},
+                {$inc: {space: -1}},
                 {safe: true, multi: false},
                 (e, result) => {
                     if (e) return next(e)
                     res.send((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'})
                 })
             })
-        
-        
-        /* app.put('/lessons/:id', async (req, res) => { */
-        /*     try { */
-        /*         const lessonId = req.params.id; */
-        /*         const { spaces } = req.body; */
-        /*         const updatedLesson = await lessonsCollection.findOneAndUpdate( */
-        /*             { _id: new ObjectID(lessonId) }, */
-        /*             { $inc: { spaces: -spaces } }, */
-        /*             { returnDocument: 'after' } */
-        /*         ); */
-        /*         res.json(updatedLesson); */
-        /*     } catch (error) { */
-        /*         console.error(error); */
-        /*         res.status(500).json({ error: 'Internal Server Error' }); */
-        /*     } */
-        /* }); */
-
-        // Delete object
-app.delete('/lessons/:id', (req,res,next)=>{
-    req.collection.deleteOne(
-        {_id: new ObjectID(req.params.id)},
-        (e,results) => {
-        if (e) return next(e)
-        res.send(results ? {msg: 'sucess'} : {msg: 'error'})
-    })
-})
-
-     // Search object
-app.get('/lessons/:id/search/:keyword', (req, res,next) => {
-    let {keyword} = req.params
-    req.collection.find({}).toArray((err, results) => {
-        if (err) return next(err)
-        let filteredList = results.filter((lesson) => {
-            return lesson.Subject.toLowerCase().match(keyword.toLowerCase()) || lesson.Location.toLowerCase().match(keyword.toLowerCase())
-        });  
-        res.send(filteredList)
-    })
-})
-
-// File middleware
-app.use(function(req,res,next) {
-    var filePath = path.join(__dirname, "images", req.url);
-    fs.stat(filePath, function(err, fileInfo) {
-        if (err) {
-            next();
-            return;
-        }
-        if (fileInfo.isFile()) {
-            res.sendFile(filePath);
-        }
-        else {
-            next();
-        }
-    });
-});
 
 
-app.use(function(req,res, next) {
-    res.status(404);
-    res.send("File not found");
-});
+            // Search object
+        app.get('/search', (req, res,next) => {
+           req.collection = db.collection("lessons")
+           let {keyword} = req.query
+           console.log("searching for "+keyword)
+           req.collection.find({}).toArray((err, results) => {
+               if (err) return next(err)
+               let filteredList = results.filter((lesson) => {
+                    return lesson.subject.toLowerCase().includes(keyword.toLowerCase()) || lesson.location.toLowerCase().includes(keyword.toLowerCase())
+                });  
+                res.send(filteredList)
+            })
+        })
+  
+
+
+
+
+         // File middleware
+        app.use(function(req,res,next) {
+             var filePath = path.join(__dirname, "Public/images", req.url);
+             fs.stat(filePath, function(err, fileInfo) {
+                 if (err) {
+                     next();
+                     return;
+                 }
+                if (fileInfo.isFile()) {
+                    res.sendFile(filePath);
+                }
+                else {
+                      next();
+                }
+            });
+        });
+
+
+        app.use(function(req,res, next) {
+             res.status(404);
+             res.send("File not found");
+        });
   
   
 
